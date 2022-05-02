ecb322ea2f50eae767a276c45aec10525e49ed8e
Aaron McUmber <aaron.mcumber@gmail.com>
2022-05-02 00:11:12 +0000

    feat: Completed DecisionTree Evaluation
    Ready for submission

--- Project_Plagiarism_Detection/source_sklearn/train.py
+++ Project_Plagiarism_Detection/source_sklearn/train.py
@@ -1,9 +1,11 @@
-# from __future__ import print_function # unecessary for >py3
+from __future__ import print_function
 
 import argparse
 import os
-import pandas as pd
 
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
@@ -29,13 +31,13 @@ def model_fn(model_dir):
     
     return model
 
-def get_model():
+def get_model(max_depth):
     return Pipeline([
         ('scaler', StandardScaler()),
-        ('tree', DecisionTreeClassifier(max_depth=4)),
+        ('tree', DecisionTreeClassifier(max_depth=max_depth)),
     ])
 
-def get_training_data(training_dir) -> tuple[np.ndarray, np.ndarray]:
+def get_training_data(training_dir):
     # MOVED to function for encapsulation
     
     train_data = pd.read_csv(os.path.join(training_dir, "train.csv"), header=None, names=None)
@@ -45,14 +47,15 @@ def get_training_data(training_dir) -> tuple[np.ndarray, np.ndarray]:
     train_x = train_data.iloc[:,1:]
     return train_x, train_y
 
-def main(train_x, train_x, model) -> None:
+def main(training_dir, model_dir, **kwargs) -> None:
     """Main Entrypoint"""
-    train_x, train_y = get_training_data(training_dir)
     ## --- Your code here --- ##
+    # Read in csv training file
+    train_x, train_y = get_training_data(training_dir)
     
 
     ## TODO: Define a model 
-    model = get_model()
+    model = get_model(**kwargs)
      
     ## TODO: Train the model
     model.fit(train_x, train_y)
@@ -61,7 +64,7 @@ def main(train_x, train_x, model) -> None:
     
 
     # Save the trained model
-    joblib.dump(model, os.path.join(args.model_dir, "model.joblib"))
+    joblib.dump(model, os.path.join(model_dir, "model.joblib"))
 
     
 ## TODO: Complete the main code
@@ -80,13 +83,10 @@ if __name__ == '__main__':
     parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
     
     ## TODO: Add any additional arguments that you will need to pass into your model
+    parser.add_argument('--max-depth', type=int, default=4, help='max depth of decision tree')
     
     # args holds all passed-in arguments
     args = parser.parse_args()
-
-    # Read in csv training file
-    training_dir = args.data_dir
     
     # --- [ACM] -- Moved data handling code into a main function
-    
-    
+    main(training_dir=args.data_dir, model_dir=args.model_dir, max_depth=args.max_depth)   
